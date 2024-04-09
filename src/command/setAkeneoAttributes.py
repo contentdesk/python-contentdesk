abc4dae26b948db40b9d1e71b419853138fc1fea
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-04-09 11:49:48 +0200

    Refactor CSV data filtering and conversion in setAkeneoAttributes.py and setAkeneoAttributesGroups.py

---
+++ src/command/setAkeneoAttributes.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Define the CSV URL
+csv_url = "https://docs.google.com/spreadsheets/d/187orB1Qx9YgeS8cVyI29DuGLhr-oj8yIiCIR5wqyqXk/gviz/tq?tqx=out:csv&sheet=setupAttribute"
+
+# Read the CSV data into a pandas DataFrame
+df = pd.read_csv(csv_url)
+
+print(df)
+# filter df by enabled = false or enabled = empty
+df = df[df["enabled"] == True]
+
+# Convert the DataFrame to a JSON object
+json_data = df.to_json(orient="records")
+
+# Write the JSON data to a file
+with open("../../output/index/Akeneo/Attributes.json", "w") as file:
+    file.write(json_data)
---
+++ src/command/setAkeneoAttributesGroups.py
@@ -0,0 +1,18 @@
+import pandas as pd
+
+# Define the CSV URL
+csv_url = "https://docs.google.com/spreadsheets/d/1HbcevTJlnt7RuoO6qedT6FY_7np-08oLpOAS54kS18E/gviz/tq?tqx=out:csv&sheet=setupAttributeGroup"
+
+# Read the CSV data into a pandas DataFrame
+df = pd.read_csv(csv_url)
+
+print(df)
+# filter df by enabled = false or enabled = empty
+df = df[df["enabled"] == True]
+
+# Convert the DataFrame to a JSON object
+json_data = df.to_json(orient="records")
+
+# Write the JSON data to a file
+with open("../../output/index/Akeneo/AttributeGroups.json", "w") as file:
+    file.write(json_data)

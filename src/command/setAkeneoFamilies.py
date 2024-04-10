0d76762d446356addee5b10dce5b2603d11005dc
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-04-10 05:24:27 +0200

    Refactor setAkeneoAttributes.py and setAkeneoFamilies.py to improve code readability and maintainability

--- src/command/setAkeneoFamilies.py
+++ src/command/setAkeneoFamilies.py
@@ -1,18 +1,29 @@
 import pandas as pd
 
-# Define the CSV URL
-csv_url = "https://docs.google.com/spreadsheets/d/1-vZI8rZxwbUVqvxU9tn5dVhZG282LXF7KvDTTvyuOfY/gviz/tq?tqx=out:csv&sheet=setupTypes"
+def readCsv(url):
+    # Read the CSV data into a pandas DataFrame
+    df = pd.read_csv(url)
+    return df
 
-# Read the CSV data into a pandas DataFrame
-df = pd.read_csv(csv_url)
+def main():
+    # Define the CSV URL
+    csv_url = "https://docs.google.com/spreadsheets/d/1-vZI8rZxwbUVqvxU9tn5dVhZG282LXF7KvDTTvyuOfY/gviz/tq?tqx=out:csv&sheet=setupTypes"
+    addition_csv_url = "https://docs.google.com/spreadsheets/d/1-vZI8rZxwbUVqvxU9tn5dVhZG282LXF7KvDTTvyuOfY/gviz/tq?tqx=out:csv&sheet=additionalTypes"
 
-print(df)
-# filter df by enabled = false or enabled = empty
-df = df[df["enabled"] == True]
+    df = readCsv(csv_url)
+    df_addition = readCsv(addition_csv_url)
+    df = pd.concat([df, df_addition])
+    # filter df by enabled = false or enabled = empty
+    df = df[df["enabled"] == True]
 
-# Convert the DataFrame to a JSON object
-json_data = df.to_json(orient="records")
+    print(df)
 
-# Write the JSON data to a file
-with open("../../output/index/akeneo/families.json", "w") as file:
-    file.write(json_data)
+    # Convert the DataFrame to a JSON object
+    json_data = df.to_json(orient="records")
+
+    # Write the JSON data to a file
+    with open("../../output/index/akeneo/families.json", "w") as file:
+        file.write(json_data)
+
+if __name__ == "__main__":
+    main()

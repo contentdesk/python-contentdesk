d355f6bb597669bbf70f5e070c854a00d0e8f9fb
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-04-30 07:36:10 +0200

    Refactor setIgnoreProperties.py to update CSV data retrieval and improve code structure

--- src/command/setIgnoreProperties.py
+++ src/command/setIgnoreProperties.py
@@ -10,22 +10,26 @@ def setJsonData(properties):
 
     return indexIgnoreProperties
 
-# Define the CSV URL
-csv_url = "https://docs.google.com/spreadsheets/d/187orB1Qx9YgeS8cVyI29DuGLhr-oj8yIiCIR5wqyqXk/gviz/tq?tqx=out:csv&sheet=setupAttribute"
+def main():
+    # Define the CSV URL
+    csv_url = "https://docs.google.com/spreadsheets/d/187orB1Qx9YgeS8cVyI29DuGLhr-oj8yIiCIR5wqyqXk/gviz/tq?tqx=out:csv&sheet=setupAttribute"
 
-# Read the CSV data into a pandas DataFrame
-df = pd.read_csv(csv_url)
+    # Read the CSV data into a pandas DataFrame
+    df = pd.read_csv(csv_url)
 
-print(df)
-# filter df by enabled = false or enabled = empty
-df = df[(df["enabled"] == False) | (df["pimType"] != "attribute")]
+    print(df)
+    # filter df by enabled = false or enabled = empty
+    df = df[(df["enabled"] == False) | (df["pimType"] != "attribute")]
 
-# Convert the DataFrame to a JSON object
-json_data = df.to_json(orient="records")
+    # Convert the DataFrame to a JSON object
+    json_data = df.to_json(orient="records")
 
-print("JSON Data: ", json_data)
-data = setJsonData(json.loads(json_data))
+    print("JSON Data: ", json_data)
+    data = setJsonData(json.loads(json_data))
 
-# Write the JSON data to a file
-with open("../../output/ignoreProperties.json", "w") as file:
-    file.write(json.dumps(data))
+    # Write the JSON data to a file
+    with open("../../output/ignoreProperties.json", "w") as file:
+        file.write(json.dumps(data))
+
+if __name__ == "__main__":
+    main()

0adf3da665c59944d90d7d566258aceef459a8c4
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-04-30 16:44:44 +0200

    Refactor patchFamilies.py to fix logical operator and update family attributes

--- src/command/patchFamilies.py
+++ src/command/patchFamilies.py
@@ -167,7 +167,7 @@ def createFamilies():
     families = getFamilies()
     for family in families:
         print ("CREATE - Family: "+ family["label"])
-        if family["enabled"] == 1 and family["type"] == None:
+        if family["enabled"] == 1 and family["type"] == None or family["type"] == "additinalTypes":
             print("patch Family: ", family["label"])
             createFamily(family)
             print("FINISH - patch Family: ", family["label"])

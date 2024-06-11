76c75ca0ae7f24508b90d0272a4b86da182277dd
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-06-11 14:38:03 +0200

    Refactor LodgingBusiness.py to remove unnecessary attributes and improve module organization

--- src/entity/Family/LodgingBusiness.py
+++ src/entity/Family/LodgingBusiness.py
@@ -1,12 +1,15 @@
-import entity.Family as Family
+import entity.Family.Family as Family
 
 def setBody(family, families):
     body = Family.setBody(family, families)
 
     if 'dayTime' in body['attributes']:
+        print("DayTime: ", body['attributes']['dayTime'])
         body['attributes'].pop('dayTime')
     
     if 'duration' in body['attributes']:
         body['attributes'].pop('duration')
+
+    print("LodgingBusiness Body: ", body)
 
     return body

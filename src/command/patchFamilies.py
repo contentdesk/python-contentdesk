46c2abf578137b20b6acdfed27e62f45b928ec20
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-06-11 08:57:39 +0200

    Refactor patchFamilies.py to optimize attribute handling and update seating attributes for MeetingRoom family

--- src/command/patchFamilies.py
+++ src/command/patchFamilies.py
@@ -5,14 +5,10 @@ from akeneo.akeneo import Akeneo
 import sys
 sys.path.append("..")
 
-from service.loadEnv import loadEnv, getEnvironment
-
-import sys
-sys.path.append("..")
-
-import setting
+from service.loadEnv import loadEnv
 import service.debug as debug
-import entity.MeetingRoom as MeetingRoom
+import entity.Family.MeetingRoom as MeetingRoom
+import service.cliArguments as cliArguments
 
 def getSettings():
     # Define the CSV URL
@@ -352,110 +348,6 @@ def createFamily(family, families, akeneo):
         debug.addToLogFile(code, response)
     return response
 
-def createFamilyMeetingRoom(family, families, akeneo):
-    code = family["label"]
-
-    if family["attribute_requirements.ecommerce"] != None:
-        attribute_requirements = {attrRequ: attrRequ for attrRequ in family["attribute_requirements.ecommerce"].split(",")}
-    else:
-        attribute_requirements = {"sku": "sku", "name": "name", "image": "image"}
-
-    #attribute_requirements = getParentAttributesRequirements(family, families, attribute_requirements)
-
-    if family["attributes"] != None:
-        attributes = {attr: attr for attr in family["attributes"].split(",")}
-    else:
-        attributes = {}
-
-    #attributes = getFamilyAttributes(code, attributes)
-    #attributes = getParentAttributes(family, families, attributes)
-    
-    attributes['seating_banquet'] = 'seating_banquet'
-    attributes['seating_bar_table'] = 'seating_bar_table'
-    attributes['seating_block'] = 'seating_block'
-    attributes['seating_boardroom'] = 'seating_boardroom'
-    attributes['seating_concert'] = 'seating_concert'
-    attributes['seating_seminar'] = 'seating_seminar'
-    attributes['seating_ushape'] = 'seating_ushape'
-
-    attributes['openstreetmap_id'] = 'openstreetmap_id'
-    attributes['license'] = 'license'
-    attributes['copyrightHolder'] = 'copyrightHolder'
-    attributes['author'] = 'author'
-
-    attributes['amenityFeature'] = 'amenityFeature'
-    attributes['occupancy'] = 'occupancy'
-    attributes['floorLevel'] = 'floorLevel'
-    attributes['floorSize'] = 'floorSize'
-    attributes['maximumAttendeeCapacity'] = 'maximumAttendeeCapacity'
-    attributes['yearBuilt'] = 'yearBuilt'
-    attributes['offers'] = 'offers'
-    attributes['priceRange'] = 'priceRange'
-
-    if 'image' in attributes:
-        attributes['image_description'] = 'image_description'
-    
-    body = {
-        "code": code,
-        "attribute_as_label": family["attribute_as_label"],
-        "attribute_as_image": family["attribute_as_image"],
-        "attribute_requirements": {
-            "ecommerce": attribute_requirements,
-        },
-        "labels": {
-            "en_US": family["label.en_US"],
-            "de_CH": family["label.de_CH"],
-            "fr_FR": family["label.fr_FR"],
-            "it_IT": family["label.it_IT"],
-        }
-    }
-
-    # Remove Properties
-    #print("Remove Attributes: ")
-    ##print(code)
-    #attributes = removeProperties(code, attributes)
-
-    body["attributes"] = attributes
-
-    clearBody = {
-        "code": code,
-        "attribute_as_label": family["attribute_as_label"],
-        "attribute_as_image": family["attribute_as_image"],
-        "attribute_requirements": {
-            "ecommerce": [
-                "sku",
-                "name",
-                "image"
-            ],
-        },
-        "labels": {
-            "en_US": family["label.en_US"],
-            "de_CH": family["label.de_CH"],
-            "fr_FR": family["label.fr_FR"],
-            "it_IT": family["label.it_IT"],
-        },
-        "attributes": [
-            "sku",
-            "name",
-            "image"
-        ]
-    }
-
-    try:
-        # Clear Attributes
-        #response = akeneo.patchFamily(code, clearBody)
-        # DEBUG - Write to file
-        addToFile(code, body)
-        # To Akeneo
-        response = akeneo.patchFamily(code, body)
-        addToLogFile(code, response)   
-    except Exception as e:
-            print("Error: ", e)
-            print("patch Family: ", code)
-            print("Response: ", response)
-            addToLogFile(code, response)
-    return response
-
 def createFamilies(target, families, importFamilies = None):
     #filter families by label = Hotel
     #families = [family for family in families if family["label"] == "Hotel"]
@@ -479,19 +371,8 @@ def createFamilies(target, families, importFamilies = None):
                 print("FINISH - patch Family: ", family["label"])
 
 def main():
-    # Set Environments
-    if sys.argv[1] == "all":
-        environments = getEnvironment()
-    else:
-        environments = [sys.argv[1]]
-    
-    # Set Import Families
-    if sys.argv[2:] == []:
-        importFamilies = None
-    else:
-        importFamilies = sys.argv[2:]
-    print("Import Families: ")
-    print(importFamilies)
+    environments = cliArguments.getEnvironment(sys)
+    importFamilies = cliArguments.getArguments(sys)
 
     # Set Familie Settings
     families = getSettings()
---
+++ src/service/cliArguments.py
@@ -0,0 +1,17 @@
+
+def getEnvironment(sys):
+    # Set Environments
+    if sys.argv[1] == "all":
+        environments = getEnvironment()
+    else:
+        environments = [sys.argv[1]]
+
+    return environments
+
+def getArguments(sys):
+    # Set Import Families
+    if sys.argv[2:] == []:
+        arguments = None
+    else:
+        arguments = sys.argv[2:]
+    return arguments

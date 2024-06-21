accf762309fd5e28362fb474ade6adee9aae46a6
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-06-21 08:29:42 +0200

    Refactor code to add amenity feature and award options

--- src/command/patchAttributeOptions.py
+++ src/command/patchAttributeOptions.py
@@ -19,13 +19,17 @@ def getAttributeOptions():
     othersUrl = "https://docs.google.com/spreadsheets/d/1J0iDoPqamlvWzBvxmZPDB0_uks0U3UuPLvizGolVeYg/gviz/tq?tqx=out:csv&sheet=others"
     starRatingUrl = "https://docs.google.com/spreadsheets/d/1VtVxnO0YfJ3ztRwcsK3elN8orGhlSRSDbkn8Whl523Y/gviz/tq?tqx=out:csv&sheet=starRating"
     servesCuisineUrl = "https://docs.google.com/spreadsheets/d/1B3yJroHLuNgIDSCDfxIY5tpX13zyn1ngigGMlNaxVgk/gviz/tq?tqx=out:csv&sheet=servesCuisine"
+    amenityFeatureUrl = "https://docs.google.com/spreadsheets/d/1eK8PI9OEfdeBPDYXegdmA6rs3hl5RcDCAUYlJ5iIrV4/gviz/tq?tqx=out:csv&sheet=amenityFeature"
+    awardUrl = "https://docs.google.com/spreadsheets/d/1XDmYmyp46la94TQ4ztPXirWzHPgw4mAwCBf61tEAYIo/gviz/tq?tqx=out:csv&sheet=award"
     # Set DataFrame
     dfOthers = readCsv(othersUrl)
     dfStarRating = readCsv(starRatingUrl)
     dfServesCuisine = readCsv(servesCuisineUrl)
+    dfAmenityFeature = readCsv(amenityFeatureUrl)
+    dfAward = readCsv(awardUrl)
 
     # Merge DataFrame
-    df = pd.concat([dfOthers, dfStarRating, dfServesCuisine])
+    df = pd.concat([dfOthers, dfStarRating, dfServesCuisine, dfAmenityFeature, dfAward])
 
     #print(df)
 

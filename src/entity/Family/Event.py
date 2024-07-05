6c894db3883548cb449fa69c3f1c2fef0da8f9ad
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-07-05 10:39:16 +0200

    Refactor code to update attributes in Family.Event.py

--- src/entity/Family/Event.py
+++ src/entity/Family/Event.py
@@ -2,5 +2,18 @@ import entity.Family.Family as Family
 
 def setBody(family, families):
     body = Family.setBody(family, families)
-
+    
+    body['attributes']['latitude'] = 'latitude'
+    body['attributes']['longitude'] = 'longitude'
+    
+    body['attributes']['streetAddress'] = 'streetAddress'
+    body['attributes']['postalCode'] = 'postalCode'
+    body['attributes']['addressLocality'] = 'addressLocality'
+    body['attributes']['addressCountry'] = 'addressCountry'
+    body['attributes']['addressRegion'] = 'addressRegion'
+    body['attributes']['tourismregion'] = 'tourismregion'
+    
+    body['attributes']['startTime'] = 'startTime'
+    body['attributes']['endTime'] = 'endTime'
+    
     return body

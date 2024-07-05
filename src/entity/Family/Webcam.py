f3c8a364d6dfe78e76e4f2b0cbc1fa678d30476e
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-07-05 15:34:10 +0200

    Refactor code to add 'Webcam' family type and attributes to the importFamilyEntity function

--- src/entity/Family/Webcam.py
+++ src/entity/Family/Webcam.py
@@ -3,7 +3,20 @@ import entity.Family.Family as Family
 def setBody(family, families):
     #body = Family.setBody(family, families)
     
+    attribute_requirements = {"sku": "sku", "name": "name", "license": "license", "embedUrl": "embedUrl"}
+    
     body = {}
+    body['code'] = family['label']
+    body['attribute_as_label'] = 'name'
+    body['attribute_as_image'] = 'image'
+    body["attribute_requirements"] = {}
+    body["attribute_requirements"]['ecommerce'] = attribute_requirements
+    body["labels"] = {
+        "en_US": family["label"],
+        "de_CH": family["label"],
+        "fr_FR": family["label"],
+        "it_IT": family["label"]
+    }
     
     body['attributes'] = {}
     body['attributes']['sku'] = 'sku'

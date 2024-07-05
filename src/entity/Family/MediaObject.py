801749035a27ca9d065f7bf37994e28a3a593629
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-07-05 09:00:19 +0200

    Refactor code to update 'contentUrl' attribute in MediaObject.py

--- src/entity/Family/MediaObject.py
+++ src/entity/Family/MediaObject.py
@@ -7,7 +7,7 @@ def setBody(family, families):
     if family["attribute_requirements.ecommerce"] != None:
         attribute_requirements = {attrRequ: attrRequ for attrRequ in family["attribute_requirements.ecommerce"].split(",")}
     else:
-        attribute_requirements = {"sku": "sku", "name": "name", "image": "image", "license": "license"}
+        attribute_requirements = {"sku": "sku", "name": "name", "license": "license", "contentUrl": "contentUrl"}
 
     attributes = {}
     
@@ -38,7 +38,5 @@ def setBody(family, families):
     body['attributes']['thumbnailUrl'] = 'thumbnailUrl'
     body['attributes']['contentUrl'] = 'contentUrl'
     body['attributes']['embedUrl'] = 'embedUrl'
-    
-    
 
     return body

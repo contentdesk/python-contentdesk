c254925195469242ce1a68f3d4e71f31b813aa98
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-08-12 15:40:44 +0200

    Refactor license.py to update license data format

--- src/migration/license.py
+++ src/migration/license.py
@@ -7,6 +7,8 @@
 
 # Upload all Products with Attribute license
 
+# https://tourismus.atlassian.net/browse/PIM-483
+
 def getProducts(target, attribute):
     #search = 'search={"openingHours_text":[{"operator":"NOT EMPTY","value":"","locale":"de_CH"}]}'
     search = '{"license":[{"operator":"IN","value":["cc_by","cc_by_sa","cc_by_nd","cc0"]}]}&attributes=license'
@@ -20,6 +22,12 @@ def transform(products):
         if "license" in product['values']:
             if product['values']["license"][0]['data'] == "cc_by":
                 product['values']["license"][0]['data'] = "ccby"
+                productsUpdated.append(product)
+            elif(product['values']["license"][0]['data'] == "cc_by_sa"):
+                product['values']["license"][0]['data'] = "ccbysa"
+                productsUpdated.append(product)
+            elif(product['values']["license"][0]['data'] == "cc_by_nd"):
+                product['values']["license"][0]['data'] = "ccbynd"
                 productsUpdated.append(product)
     
     return productsUpdated

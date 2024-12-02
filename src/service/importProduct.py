ca058a06af857e6ce1d2685b56b43a32c35f6787
Thomas Solenthaler <tsolenthaler@gmail.com>
2024-12-02 13:32:55 +0100

    Refactor transform function: streamline product value assignment and enhance dynamic attribute handling

--- src/service/importProduct.py
+++ src/service/importProduct.py
@@ -29,65 +29,24 @@ def transform(products):
             transformed_product['enabled'] = product['enabled']
         # VALUES
         transformed_product['values'] = {}
-        # NAME
-        transformed_product['values']['name'] = []
-        if 'name-de_CH' in product:
-            transformed_product['values']['name'].append({
-                'locale': 'de_CH',
-                'scope': None,
-                'data': product['name-de_CH']
-            })
-        if 'name-en_US' in product:
-            transformed_product['values']['name'].append({
-                'locale': 'en_US',
-                'scope': None,
-                'data': product['name-en_US']
-            })
-        # DESCRIPTION
-        transformed_product['values']['description'] = []
-        if 'description-de_CH-ecommerce' in product:
-            transformed_product['values']['description'].append({
-                'locale': 'de_CH',
-                'scope': 'ecommerce',
-                'data': product['description-de_CH']
-            })
-        if 'description-en_US-ecommerce' in product:
-            transformed_product['values']['description'].append({
-                'locale': 'en_US',
-                'scope': 'ecommerce',
-                'data': product['description-en_US']
-            })
-        if 'description-fr_FR-ecommerce' in product:
-            transformed_product['values']['description'].append({
-                'locale': 'fr_FR',
-                'scope': 'ecommerce',
-                'data': product['description-fr_FR']
-            })
-        if 'description-it_IT-ecommerce' in product:
-            transformed_product['values']['description'].append({
-                'locale': 'it_IT',
-                'scope': 'ecommerce',
-                'data': product['description-it_IT']
-            })
-        
         # dynamic Attribut
         for key, value in product.items():
             print (key)
             if '-' in key:
                 parts = key.split('-')
                 if len(parts) == 2:
-                    attribute, locale = parts
-                    transformed_product['values'].setdefault(attribute, []).append({
-                    'locale': locale,
-                    'scope': None,
-                    'data': value
+                        attribute, locale = parts
+                        transformed_product['values'].setdefault(attribute, []).append({
+                        'locale': locale,
+                        'scope': None,
+                        'data': value
                     })
                 elif len(parts) == 3:
-                    attribute, locale, scope = parts
-                    transformed_product['values'].setdefault(attribute, []).append({
-                    'locale': locale,
-                    'scope': scope,
-                    'data': value
+                        attribute, locale, scope = parts
+                        transformed_product['values'].setdefault(attribute, []).append({
+                        'locale': locale,
+                        'scope': scope,
+                        'data': value
                     })
         
         transformed_products.append(transformed_product)

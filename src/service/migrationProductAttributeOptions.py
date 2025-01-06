dbdd53fee6f1891c66dfcedf3a2bfe687e3dfd68
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-01-06 11:31:28 +0100

    Refactor migration logic to streamline product patching process and enhance argument handling

--- src/service/migrationProductAttributeOptions.py
+++ src/service/migrationProductAttributeOptions.py
@@ -1,20 +1,18 @@
 import service.debug as debug
+import migration.AttributeOptions as migration
 
-def importMigrationSettings(attribute):
-    if(attribute == "AttributeOptions"):
-        import migration.AttributeOptions as migration
-    return migration
-
-def main(environment, target, attributes):
-    for attribute in attributes:
-        print("START PATCH PRODUCTS for: ", attribute)
-        migration = importMigrationSettings(attribute)
-        print("Get Products")
-        products = migration.getProducts(target)
-        debug.addToFileMigration(environment, attribute, 'products', products)
-        print("Transform Products")
-        productsTranform = migration.transform(products)
-        debug.addToFileMigration(environment, attribute, 'transform', productsTranform)
-        print("Upload Products")
-        productsUpload = migration.uploadProducts(target, productsTranform)
-        print("FINISH PATCH PRODUCTS for: ", attribute)
+def main(environment, target, aguments):
+    print("START PATCH PRODUCTS")
+    print("Arguments: ", aguments)
+    attribute = aguments[0]
+    oldOption = aguments[1]
+    newOptions = aguments[2:]
+    print("Get Products")
+    products = migration.getProducts(target, attribute, oldOption)
+    debug.addToFileMigration(environment, attribute, 'products', products)
+    print("Transform Products")
+    productsTranform = migration.transform(products, attribute, oldOption, newOptions)
+    debug.addToFileMigration(environment, attribute, 'transform', productsTranform)
+    print("Upload Products")
+    productsUpload = migration.uploadProducts(target, productsTranform)
+    print("FINISH PATCH PRODUCTS for: ", attribute)

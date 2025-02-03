f10a0dd2a31bc606aeca4f8db6729e96b267cc75
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-02-03 13:55:06 +0100

    Add product export functionality with CSV output for specified families

---
+++ src/command/exportProduct.py
@@ -0,0 +1,26 @@
+import json
+import requests
+import pandas as pd
+from akeneo.akeneo import Akeneo
+import sys
+sys.path.append("..")
+
+from service.loadEnv import loadEnv
+import service.debug as debug
+import service.cliArguments as cliArguments
+import service.exportProduct as export
+
+def main():
+    environments = cliArguments.getEnvironment(sys)
+    arguments = cliArguments.getArguments(sys)
+
+    print("START Export PRODUCTS")
+    for environment in environments:
+        targetCon = loadEnv(environment)
+        target = Akeneo(targetCon["host"], targetCon["clientId"], targetCon["secret"], targetCon["user"], targetCon["passwd"])
+        export.main(environment, target, arguments)
+        
+    print("FINISH Export PRODUCTS")
+
+if __name__ == '__main__':
+    main()

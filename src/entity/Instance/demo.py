f4b83304c36e10acb28704916d41e42a83907d17
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-04-02 13:22:14 +0200

    Add getProperties function to demo.py and ziggy.py with custom attributes

---
+++ src/entity/Instance/demo.py
@@ -0,0 +1,8 @@
+def getProperties():
+    properties = {}
+    
+    # Add spezific properties to all Types
+    properties['demoCustomAttribute1'] = 'demoCustomAttribute1'
+    
+    return properties
+    

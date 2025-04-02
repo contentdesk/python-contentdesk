07ae9d5d906aa21d56e2e30b51d7c335e9c3d0ef
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-04-02 13:59:27 +0200

    Add hlt_control property to getProperties function

--- src/entity/Instance/hlt.py
+++ src/entity/Instance/hlt.py
@@ -8,6 +8,8 @@ def getProperties():
     properties['mainEntityOfPage'] = 'mainEntityOfPage'
     properties['hlt_onboarding_mailing'] = 'hlt_onboarding_mailing'
     properties['hlt_onboarding_code'] = 'hlt_onboarding_code'
+    properties['hlt_control'] = 'hlt_control'
+    
     
     return properties
     

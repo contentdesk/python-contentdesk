7786a068484a5a0763577a924a269ce0a4ea821f
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-04-02 10:25:09 +0200

    Add getProperties function to hlt.py for property management

---
+++ src/entity/Instance/hlt.py
@@ -0,0 +1,10 @@
+
+
+def getProperties():
+    properties = {}
+    
+    # Add spezific properties to all Types
+    # properties['currenciesAccepted'] = 'currenciesAccepted'
+    
+    return properties
+    

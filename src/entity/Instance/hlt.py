a05e35d732940e2a3a7eca4e4a54d2ef8b96df62
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-04-04 07:08:48 +0200

    Add hlt_onlboarding_formulartyp property to getProperties function

--- src/entity/Instance/hlt.py
+++ src/entity/Instance/hlt.py
@@ -8,6 +8,7 @@ def getProperties():
     properties['mainEntityOfPage'] = 'mainEntityOfPage'
     properties['hlt_onboarding_mailing'] = 'hlt_onboarding_mailing'
     properties['hlt_onboarding_code'] = 'hlt_onboarding_code'
+    properties['hlt_onlboarding_formulartyp'] = 'hlt_onlboarding_formulartyp'
     properties['hlt_control'] = 'hlt_control'
     
     return properties

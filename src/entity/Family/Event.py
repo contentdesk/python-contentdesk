529ad83978a801d00e1f451fe36a02c18ae05db9
Thomas Solenthaler <tsolenthaler@gmail.com>
2025-10-21 13:26:50 +0200

    Add image scope and description attributes to event body

--- src/entity/Family/Event.py
+++ src/entity/Family/Event.py
@@ -45,6 +45,28 @@ def setBody(family, families):
     
     body['attributes']['logo'] = 'logo'
     
+    body['attributes']['image_01_scope'] = 'image_01_scope'
+    body['attributes']['image_02_scope'] = 'image_02_scope'
+    body['attributes']['image_03_scope'] = 'image_03_scope'
+    body['attributes']['image_04_scope'] = 'image_04_scope'
+    body['attributes']['image_05_scope'] = 'image_05_scope'
+    body['attributes']['image_06_scope'] = 'image_06_scope'
+    body['attributes']['image_07_scope'] = 'image_07_scope'
+    body['attributes']['image_08_scope'] = 'image_08_scope'
+    body['attributes']['image_09_scope'] = 'image_09_scope'
+    body['attributes']['image_10_scope'] = 'image_10_scope'
+    
+    body['attributes']['image_01_scope_description'] = 'image_01_scope_description'
+    body['attributes']['image_02_scope_description'] = 'image_02_scope_description'
+    body['attributes']['image_03_scope_description'] = 'image_03_scope_description'
+    body['attributes']['image_04_scope_description'] = 'image_04_scope_description'
+    body['attributes']['image_05_scope_description'] = 'image_05_scope_description'
+    body['attributes']['image_06_scope_description'] = 'image_06_scope_description'
+    body['attributes']['image_07_scope_description'] = 'image_07_scope_description'
+    body['attributes']['image_08_scope_description'] = 'image_08_scope_description'
+    body['attributes']['image_09_scope_description'] = 'image_09_scope_description'
+    body['attributes']['image_10_scope_description'] = 'image_10_scope_description'
+    
     body['attributes']['metaTitle'] = 'metaTitle'
     body['attributes']['metaDescription'] = 'metaDescription'
     body['attributes']['canonicalUrl'] = 'canonicalUrl'
